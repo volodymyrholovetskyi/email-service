f4a246428e07d1fb234413a2b1fdc959d858bae9
Volodymyr Holovetskyi <vholovetskyi@gmail.com>
2024-06-08 17:11:34 +0200

    Start commit

---
+++ src/test/java/ua/vholovetskyi/emailservice/EmailserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.vholovetskyi.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
